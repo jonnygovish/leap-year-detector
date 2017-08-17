1d55385b5f7ad634323572720bec2ed00ce9cdd9
John <jonnygovish@gmail.com>
2017-08-17 15:49:08 +0300

    Create Leap year function create the first behaviour that returns false when the year is not a leap year. create the second behaviour that returns true when the year is divisible by 4 and there's no remainder

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+//back-end logic
+var leapYear = function(year) {
+  if (year % 4 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+
+// front-end logic
+$(document).ready(function() {
+  $("form#leap-year").submit(function(event) {
+    event.preventDefault();
+    var year = parseInt($("input#year").val());
+    var result = leapYear(year);
+    $("#result").text(result);
+  });
+});
